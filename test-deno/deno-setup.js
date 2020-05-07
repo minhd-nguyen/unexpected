313ef7097789ec65453b3fa96fc975b00a8a15e1
Alex J Burke <alex@alexjeffburke.com>
2020-05-07 13:22:44 +0200

    Define a location global with enough to get mocha going.

--- test-deno/deno-setup.js
+++ test-deno/deno-setup.js
@@ -2,6 +2,10 @@ import '../node_modules/mocha/mocha.js';
 import unexpected from '../unexpected.esm.js';
 import unexpectedMagicPen from '../node_modules/unexpected-magicpen/unexpected-magicpen.esm.js';
 
+window.location = {
+  search: '',
+};
+
 window.mocha.reporter('spec');
 window.mocha.setup('bdd');
 
